de253f1928f011bc62c9082d4bd30809e52b201d
jyeonjyeoni <306803889+jyeonjyeoni@users.noreply.github.com>
2026-07-20 00:20:06 +0900

    웹 UI 디자인 전면 개선

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,12 @@
-const CACHE='pokedoro-web-v1';
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./','./index.html','./manifest.webmanifest','./assets/meadow.jpg','./assets/exploration-background.jpg','./assets/timer-finished.mp3','./assets/national-pokedex.json','./assets/pokemon-species-names.csv']))));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html'))))});
+const CACHE='pokedoro-web-v2';
+const CORE=['./manifest.webmanifest','./assets/meadow.jpg','./assets/exploration-background.jpg','./assets/timer-finished.mp3','./assets/national-pokedex.json','./assets/pokemon-species-names.csv'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',copy));return response}).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response})));
+});
